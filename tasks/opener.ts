943061517bec5e7c5a5b6e8fbc28530f9f2ab73c
Kazuki.Dev <rice.book.spin0930es@gmail.com>
2023-08-12 21:38:40 +0900

    Update: packages and lint files

--- tasks/opener.ts
+++ tasks/opener.ts
@@ -38,23 +38,17 @@ const openUrl = () => {
   if (FLAG === 'order_cancelled') {
     targetUrl = `${ADMIN_URL}email_templates/order_cancelled/edit`
   }
+  if (FLAG === 'order_payment_receipt') {
+    targetUrl = `${ADMIN_URL}email_templates/order_payment_receipt/edit`
+  }
   if (FLAG === 'refund_notification') {
     targetUrl = `${ADMIN_URL}email_templates/refund_notification/edit`
   }
   if (FLAG === 'draft_order_invoice') {
     targetUrl = `${ADMIN_URL}email_templates/draft_order_invoice/edit`
   }
-  if (FLAG === 'buy_online') {
-    targetUrl = `${ADMIN_URL}email_templates/buy_online/edit`
-  }
   if (FLAG === 'abandoned_checkout_notification') {
     targetUrl = `${ADMIN_URL}email_templates/abandoned_checkout_notification/edit`
-  }
-  if (FLAG === 'pos_exchange_receipt') {
-    targetUrl = `${ADMIN_URL}email_templates/pos_exchange_receipt/edit`
-  }
-  if (FLAG === 'pos_exchange_v2_receipt') {
-    targetUrl = `${ADMIN_URL}email_templates/pos_exchange_v2_receipt/edit`
   }
   if (FLAG === 'gift_card_notification') {
     targetUrl = `${ADMIN_URL}email_templates/gift_card_notification/edit`
